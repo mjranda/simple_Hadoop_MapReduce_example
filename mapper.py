f1ddc26e4a0939108ca569505fd1ca8a684990ec
mjranda <mary.j.randa@gmail.com>
2020-01-24 03:20:02 +0000

    lowercase text and remove stopwords

--- mapper.py
+++ mapper.py
@@ -5,10 +5,16 @@ import sys
 for line in sys.stdin:
     # remove leading and trailing whitespace
     line = line.strip()
+    line = line.lower()
 
     # split the line into words; splits on any whitespace
     words = line.split()
 
+
+    #set stopwords
+    stopwords = set(['the','and','I','an','a','in','t','my','of','is','s','am','or'])
+
     # output tuples (word, 1) in tab-delimited format
     for word in words:
-        print '%s\t%s' % (word, "1")
+	if word not in stopwords:
+           print '%s\t%s' % (word, "1")
